4bee77daf961a6768a888eb45170ae11c2ae0774
Avay Azad <avay.azad@nagarro.com>
2021-11-23 17:02:31 +0530

    add 1st unit test for label controller

--- AdForm.Backend/UnitTest/AdForm.Backend.Test/ControllersTests/BaseController.cs
+++ AdForm.Backend/UnitTest/AdForm.Backend.Test/ControllersTests/BaseController.cs
@@ -1,29 +1,42 @@
-﻿namespace AdForm.Backend.Test
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using ToDoApp.Business;
+
+namespace AdForm.Backend.Test
 {
     public class BaseController : MapperInitiator
     {
-        //public ControllerContext Context { get; }
-        //public ApiVersion Version { get; }
-        //public Mock<ILableAppService> LabelService { get; }
-        //public Mock<IToDoItemAppService> ToDoItemService { get; }
-        //public Mock<IToDoListAppService> ToDoListService { get; }
-        //public Mock<IUserAppService> UserService { get; }
+        public ControllerContext Context { get; }
+        public ApiVersion Version { get; }
+        public Mock<ILableAppService> LabelService { get; }
+        public Mock<IToDoItemAppService> ToDoItemService { get; }
+        public Mock<IToDoListAppService> ToDoListService { get; }
+        public Mock<IUserAppService> UserService { get; }
 
-        //protected BaseController()
-        //{
-        //    Context = new ControllerContext
-        //    {
-        //        HttpContext = new DefaultHttpContext()
-        //    };
+      private readonly LabelResponseDto _labelResponseDto = new LabelResponseDto { LabelId =1};
 
-        //    Context.HttpContext.Request.HttpContext.Items["UserId"] = "1";
-        //    Version = new ApiVersion(1, 0);
-        //    LabelService = new Mock<ILableAppService>();
-        //    ToDoItemService = new Mock<IToDoItemAppService>();
-        //    ToDoListService = new Mock<IToDoListAppService>();
-        //    UserService = new Mock<IUserAppService>();
-           
-        //}
+        protected BaseController()
+        {
+            Context = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            Context.HttpContext.Request.HttpContext.Items["UserId"] = "1";
+            Version = new ApiVersion(1, 0);
+            LabelService = new Mock<ILableAppService>();
+            ToDoItemService = new Mock<IToDoItemAppService>();
+            ToDoListService = new Mock<IToDoListAppService>();
+            UserService = new Mock<IUserAppService>();
+
+
+            //Mock methods
+            LabelService.Setup(p => p.CreateAsync(It.IsAny<LabelRequestDto>())).Returns(Task.FromResult(_labelResponseDto));
+            
+
+        }
 
     }
 }
---
+++ AdForm.Backend/UnitTest/AdForm.Backend.Test/ControllersTests/LabelControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using ToDoApp.Api.Controllers.V1;
+using ToDoApp.Business;
+
+namespace AdForm.Backend.Test.ControllersTests
+{
+    public class LabelControllerTests : BaseController
+    {
+        private LabelController controller;
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            controller = new LabelController(LabelService.Object)
+            {
+                ControllerContext = Context
+            };
+        }
+
+        /// <summary>
+        /// Add label test.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task AddLabelTest()
+        {
+            IActionResult result = await controller.Post(new LabelRequestDto { Name = "Labeltest" }, new CreateLabelRequestValidator { });
+            CreatedAtActionResult response = result as CreatedAtActionResult;
+            Assert.AreEqual(StatusCodes.Status201Created, (int)response.StatusCode);
+        }
+    }
+}
