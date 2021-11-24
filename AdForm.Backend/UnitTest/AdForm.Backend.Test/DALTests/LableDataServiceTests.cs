06596f328250c387a3941a92d3e6ef6d25df1e55
Avay Azad <avay.azad@nagarro.com>
2021-11-24 19:31:39 +0530

    added Label database service test

---
+++ AdForm.Backend/UnitTest/AdForm.Backend.Test/DALTests/LableDataServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using AdForm.DBService;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ToDoApp.DataService;
+
+namespace AdForm.Backend.Test.DALTests
+{
+    public class LableDataServiceTests : ToDoAppDbContextInitiator
+    {
+        private readonly ILabelDataService _labelDataService;
+
+        public LableDataServiceTests()
+        {
+            _labelDataService = new LabelDataService(DBContext);
+            DBContext.Labels.Add(new Labels
+            {
+                Name = "something",
+                UserId = 1,
+            });
+            DBContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get labels test.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetLabelsTest()
+        {
+            List<Labels> LabelList = await _labelDataService.GetAllAsync(1);
+            int count = LabelList.Count;
+            Assert.IsNotNull(LabelList);
+            Assert.IsTrue(count >= 1);
+        }
+
+        /// <summary>
+        /// Get label by lable id test.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetLabelByIdTest()
+        {
+            Labels label = await _labelDataService.GetByIdAsync(1, 1);
+            Assert.IsNotNull(label);
+            Assert.IsTrue(label.LabelId == 1);
+        }
+        /// <summary>
+        /// Get label by lable Name test.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task GetLabelByNameTest()
+        {
+            Labels label = await _labelDataService.GetByNameAsync("something", 1);
+            Assert.IsNotNull(label);
+            Assert.IsTrue(label.Name == "something");
+        }
+        /// <summary>
+        /// Add label test.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task AddLabelTest()
+        {
+            Labels addedLabel = await _labelDataService.AddAsync(new Labels { Name = "blue", UserId = 1 });
+            Assert.IsNotNull(addedLabel);
+            Assert.AreEqual("blue", addedLabel.Name);
+        }
+        /// <summary>
+        /// test to delete existing Label record.
+        /// </summary>
+        [Test]
+        public async Task DeleteLabelTest()
+        {
+            Labels label = await _labelDataService.GetByNameAsync("blue", 1);
+            Labels deletedLabel = await _labelDataService.DeleteAsync(label);
+            Assert.IsNotNull(deletedLabel);
+            Assert.AreEqual("blue", deletedLabel.Name);
+        }
+    }
+}
---
+++ AdForm.Backend/UnitTest/AdForm.Backend.Test/DALTests/ToDoAppDbContextInitiator.cs
@@ -0,0 +1,21 @@
+﻿using AdForm.DBService;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdForm.Backend.Test.DALTests
+{
+    public class ToDoAppDbContextInitiator : MapperInitiator
+    {
+        public HomeworkDBContext DBContext { get; }
+        public ToDoAppDbContextInitiator()
+        {
+            DbContextOptionsBuilder<HomeworkDBContext> builder = new DbContextOptionsBuilder<HomeworkDBContext>()
+                .UseInMemoryDatabase(databaseName: "ToDoAppDb");
+
+            HomeworkDBContext _toDoDbContext = new HomeworkDBContext(builder.Options);
+            DBContext = _toDoDbContext;
+        }
+    }
+}
